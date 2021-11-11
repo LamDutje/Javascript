ddbbb35e37b57b33502869c497c06dff8ed2ecde
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-11 14:35:37 +0100

    Function Basics

---
+++ function.js
@@ -0,0 +1,26 @@
+//Pizza
+let pizzaRecipe = function () {
+    console.log('Add dough');
+    console.log('Add water');
+    console.log('Mix together');
+}
+pizzaRecipe(1);
+pizzaRecipe(2);
+pizzaRecipe(3);
+
+//Sushi
+let sushiRecipe = function () {
+    console.log('Add rice');
+    console.log('Add water');
+    console.log('Mix together');
+}
+sushiRecipe(1);
+sushiRecipe(2);
+sushiRecipe(3);
+//Change code for sushi 3x & pizza 2x
+sushiRecipe(1);
+sushiRecipe(2);
+sushiRecipe(3);
+
+pizzaRecipe(1);
+pizzaRecipe(2);
