00430d2f2351743b20e91b7b7ea859812e899957
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-11 17:26:35 +0100

    Function Arguments

--- functionArguments.js
+++ functionArguments.js
@@ -1,17 +1,15 @@
 let paint = function (color1, color2) {
-    console.log('The wall has been painted ' + color1);
-
-    console.log('The south-east wall has been painted ' + color2);
+    console.log('The wall has been painted ' + color1 + ' The south-east wall has been painted ' + color2);
 }
 
-let colorChoiceA = 'Green'
-let colorChoiceB = 'Orange'
 
 
-paint(colorChoiceA);
+paint('Red');
 //Call function second time with another color
-paint(colorChoiceB);
+paint('Blue');
 /*What happens if we call the function without an argument?
 - Doesn't execute correctly*/
 
-//Multiple arguments???
+//Multiple arguments
+paint("Red", "Blue");
+paint("Grey", "Yellow");
