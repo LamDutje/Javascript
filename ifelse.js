fc58c94a2be146b9ccc63a4e7424594b8b6e24a2
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-11 11:57:03 +0100

    If/else statements

---
+++ ifelse.js
@@ -0,0 +1,24 @@
+const age = 12;
+if (age >= 18) {
+    console.log('Allowed for entrance')
+}
+else {
+    console.log('NOT ALLOWED!!')
+}
+/////////////////////////////////////////////////////
+
+const isFemale = true;
+if (isFemale) {
+    console.log('Is a women')
+}
+else {
+    console.log('IS A MAN!!')
+}
+/////////////////////////////////////////////////////
+const driverStatus = 'bob';
+if (driverStatus === 'bob') {
+    console.log('Allowed to drive')
+}
+else {
+    console.log('NOT ALLOWED TO DRIVE!!')
+}
