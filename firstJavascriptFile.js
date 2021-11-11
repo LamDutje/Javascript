3ae5d5e04630653d6d5f5b66a6694ff926ab21d3
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-11 11:55:26 +0100

    Opdracht Gebruik je eerste Javascript

---
+++ firstJavascriptFile.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy")
+let name = 'Lam';
+console.log(name);
+// You cannot create a variable with the name if.
+let num = 4 + 4;
+console.log(num);
+let num1 = 1 + 1;
+console.log(num1);
+let num2 = 2 - 2;
+console.log(num2);
+let num3 = 3 * 3;
+console.log(num3);
+let num4 = 30 / 4;
+console.log(num4);
+let num5 = 10 % 30;
+console.log(num5);
+let age = 29;
+console.log(typeof age);
+let ageString = '29';
+console.log(typeof ageString);
