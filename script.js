68ed060423815c52cb7b58d9aa3fac3b2e55602b
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-11 11:56:12 +0100

    Opdracht Javascript draaien + comments

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy students");
+//This is a large sum
+
+/*
+let num1 = 1230941 * 1823794;
+console.log(num1);
+
+let num2 = 637263 / 54;
+console.log(num2);
+*/
+
+//Do not see comment because terminal doesnt show comment/its commented out.
+//Do not see comment because the whole block is now commented out.
