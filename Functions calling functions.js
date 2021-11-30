58ae2409bf5b6cf43a286c02f8161a94a79c9ff3
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-30 12:52:37 +0100

    Functions calling functions

--- Functions calling functions.js
+++ Functions calling functions.js
@@ -2,26 +2,45 @@
 let ageCheck = function (age) {
     if (age >= 18) {
         return true;
+    } else {
+        return false;
     }
-    return false;
 };
 //////////////////////////////////////
-let ageCheckSecond = function () {
-    if (ageCheck(15)) {
+let ageCheckSecond = function (age) {
+    if (ageCheck(age)) {
         return 'Hello there';
+    } else {
+        return 'Hey Kiddo';
     }
-    return 'Hey Kiddo';
 };
-console.log(ageCheckSecond());
+console.log(ageCheckSecond(20));
+console.log(ageCheckSecond(14));
 
-//VAT calculations
-//VAT price
-let vat = function (price) {
-    price * 0.21;
+//first VAT
+const calculateVAT = function (basePrice, VATPercentage) {
+    return basePrice * (VATPercentage / 100);
 };
 
-//VAT price included
-let vatSecond = function () {
-    vat(100) * 1.21;
-}
-console.log(vatSecond());
+const calculatePriceIncludingVAT = function (basePrice, VATPercentage) {
+    const VAT = calculateVAT(basePrice, VATPercentage);
+    return basePrice + VAT;
+};
+
+console.log(calculatePriceIncludingVAT(1000, 21)); // 1210
+console.log(calculatePriceIncludingVAT(2, 9)); // 2.18
+
+//second VAT
+const calculateBasePrice = function (priceIncludingVAT, VATPercentage) {
+    const basePrice = priceIncludingVAT / ((100 + VATPercentage) / 100);
+    return basePrice;
+};
+
+const calculateBasePriceAndVAT = function (priceIncludingVAT, VATPercentage) {
+    const basePrice = calculateBasePrice(priceIncludingVAT, VATPercentage);
+    const VAT = priceIncludingVAT - basePrice;
+    return [basePrice, VAT];
+};
+
+console.log(calculateBasePriceAndVAT(1210, 21)); // [1000, 210]
+console.log(calculateBasePriceAndVAT(2.18, 9)); // [2, 0.18]
