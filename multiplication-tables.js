99f9d446e71a7b80618a2414f26e7aaf48db4e01
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-30 17:20:14 +0100

    For loops

---
+++ multiplication-tables.js
@@ -0,0 +1,14 @@
+// Multiplication tables
+for (let i = 0; i <= 10; i++) {
+    console.log(i * 9);
+}
+
+// 1 x 9 = 9 
+// 2 x 9 = 18 
+// 3 x 9 = 27 
+// etc...
+
+// Bonus 
+for (let i = 1; i <= 10; i++) {
+    console.log(i * 1);
+}
